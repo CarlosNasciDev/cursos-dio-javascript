662b7b2f88f8ae4aaf037f7d8549f804a24a8d6d
CarlosNasciDev <104003388+CarlosNasciDev@users.noreply.github.com>
2022-07-15 10:30:52 -0300

    update

---
+++ aula 2/script.js
@@ -0,0 +1,13 @@
+//tipos primitivos
+
+//boolean
+var vouf = false
+console.log(typeof(vouf))
+
+//number
+var numero = 1
+console.log (typeof(numero))
+
+//string
+var nome = 'Carlos'
+console.log (typeof('Carlos'))
