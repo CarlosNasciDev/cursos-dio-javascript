9ba69b14771e9bd53dd3764901b5b507ef35eac8
CarlosNasciDev <104003388+CarlosNasciDev@users.noreply.github.com>
2022-07-18 13:35:11 -0300

    update

---
+++ aula 6/script.js
@@ -0,0 +1,13 @@
+function botao(){
+    document.getElementById('obrigado').innerHTML= 'obrigado por clicar'
+    
+}
+
+
+
+
+
+/*var nome = 'Carlos'
+var idade = 31
+var idade2 = 15
+alert(idade2 + idade)*/
